87fa4e7d2c991f704421d08e0eba367d6b10ac1a
Mary <7333581>
2022-08-04 08:51:13 +0400

    Spring дз

---
+++ src/main/java/pro/sky/checkout/CheckoutController.java
@@ -0,0 +1,28 @@
+package pro.sky.checkout;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
+public class CheckoutController {
+    private final CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @GetMapping(path = "/add")
+    public void addItems(@RequestParam(value = "items") List<Integer> items){
+        checkoutService.add(items);
+    }
+
+    @GetMapping(path = "/get", headers = "Accept=application/json")
+    public List<Item> getItems(){
+        return checkoutService.getCart();
+    }
+}
---
+++ src/main/java/pro/sky/checkout/Item.java
@@ -0,0 +1,17 @@
+package pro.sky.checkout;
+
+public class Item {
+    private Integer id;
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
